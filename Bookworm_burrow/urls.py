fe82c315b4abc0499ffecf15cab6411d7071ffef
Khaled Karim <kskarim177@gmail.com>
2024-09-18 19:25:59 +0600

    Core app created and contents handled

--- Bookworm_burrow/urls.py
+++ Bookworm_burrow/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from core.views import Home
 
 urlpatterns = [
+    path('',Home.as_view()),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
