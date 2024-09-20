c459ab9ab23825eca2000b5d3e59f3ea6b3e42ed
Khaled Karim <kskarim177@gmail.com>
2024-09-20 22:14:27 +0600

    Login and Logout View added

--- Bookworm_burrow/urls.py
+++ Bookworm_burrow/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from core.views import Home
 
 urlpatterns = [
-    path('',Home.as_view()),
+    path('', Home.as_view(),name = 'home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
-from .views import UserRegistrationView
+from django.urls import path
+from .views import UserRegistrationView,UserLoginView,UserLogoutView
 from django.urls import path,include
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(),name = 'register'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
