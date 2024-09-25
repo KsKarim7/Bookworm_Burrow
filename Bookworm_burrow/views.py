c8758cef1130fc4c3308fc4f1d21f481a0106a92
Khaled Karim <kskarim177@gmail.com>
2024-09-25 07:30:26 +0600

    Slug issue solved

--- Bookworm_burrow/views.py
+++ Bookworm_burrow/views.py
@@ -16,19 +16,24 @@ from categories.models import Category
 
 
 def home(request,category_slug = None):
-    # data = Post.objects.all()
-    # if(category_slug is not None):
-    #     category = Category.objects.get(slug = category_slug)
-    #     data = Post.objects.filter(category = category)
-    # categories = Category.objects.all()
-    # # print(data)
-    # return render(request, 'index.html', {'data': data, 'category': categories})
-
-
     data = Post.objects.all() 
     if category_slug is not None: 
         category = Category.objects.get(slug = category_slug) 
         data = Post.objects.filter(category  = category) 
     categories = Category.objects.all() 
     return render(request, 'index.html', {'data' : data, 'category' : categories})
+
+    data = Post.objects.all()
+    if category_slug is not None:
+        category = Category.objects.get(slug = category_slug)
+        data = Post.objects.filter(category  = category)
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'data' : data, 'category' : categories})
+
+    cars = Car.objects.all()
+    if(brand_slug is not None):
+        brand = Brand.objects.get(slug = brand_slug)
+        cars = Car.objects.filter(brand = brand)
+    brands = Brand.objects.all()
+    return render(req, 'home.html', {'car' : cars, 'brand' : brands})
     
