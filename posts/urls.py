d03edad6146f98a4f8a0ad5e0b7ff6ed36ac015c
Khaled Karim <kskarim177@gmail.com>
2025-01-04 23:55:55 +0600

    add post

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    # path('add/', views.add_post, name='add_post'),
-    # path('edit/<int:id>', views.edit_post, name='edit_post'),
+    path('add/',views.AddPostCreateView.as_view(),name='add_post'),
     path('details/<int:id>/', views.DetailBookView.as_view(), name='detail_post'),
     
     path('borrow/<int:id>/', views.Borrow_Book, name="borrow_book"), 
--- posts/views.py
+++ posts/views.py
@@ -12,6 +12,9 @@ from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.template.loader import render_to_string
 from datetime import datetime
 # from library.models import BookModel
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
 
 def send_transaction_email(user, amount, subject, template):
         message = render_to_string(template, {
@@ -46,6 +49,16 @@ def send_transaction_email(user, amount, subject, template):
 #         if post_form.is_valid():
 #             post_form.save() 
 #             return redirect('homepage') 
+
+@method_decorator(login_required, name='dispatch')
+class AddPostCreateView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('add_post')
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class DetailBookView(DetailView):
